4b4e3da5408ebb5d9d55f2e3a23b982b961c069a
lucianolivares <lucianolivares@gmail.com>
2023-01-23 19:25:12 -0300

    404 error provisorio

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, useRouteError, NavLink } from 'react-router-dom'
 import Home from './routes/home'
 import Conectividad from './routes/conectividad'
 import Root from './routes/root'
@@ -8,10 +8,22 @@ import Seguridad from './routes/seguridad'
 import Domotica from './routes/domotica'
 import Nosotros from './routes/nosotros'
 
+function ErrorBoundary () {
+  const error = useRouteError()
+
+  return (
+    <div>
+      <h2>Error {error.status}, esta página no existe.</h2>
+      <p>Porfavor ir a: <NavLink to='/'>Inicio</NavLink></p>
+    </div>
+  )
+}
+
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorBoundary />,
     children: [
       {
         path: '',
